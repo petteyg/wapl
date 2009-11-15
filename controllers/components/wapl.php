7f233ac066935c3fee31c5347dd6c7cc7a422682
Gordon Pettey <petteyg359@gmail.com>
2009-11-15 15:33:22 -0500

    Moved all WAPL communication to Component

--- controllers/components/wapl.php
+++ controllers/components/wapl.php
@@ -3,61 +3,75 @@ class WaplComponent extends Object {
 
 	var $components = array('RequestHandler', 'Session');
 	var $devKey;
-	var $force = false;
-	var $path = true;
-	var $test = false;
+	var $settings = array('force' => false, 'path' => true, 'test' => false);
+	var $Wapl;
+	var $params;
+	var $url = 'http://webservices.wapl/net/';
 
 	function initialize(&$controller, $settings = array()) {
 		$this->controller =& $controller;
 		$this->devKey = $settings['devKey'];
-		if (array_key_exists('force', $settings)) {
-			$this->force = $settings['force'];
-		}
-		if (array_key_exists('path', $settings)) {
-			$this->path = $settings['path'];
-		}
-		if (array_key_exists('test', $settings)) {
-			$this->test = $settings['test'];
-		}
+		$this->settings = array_merge($this->settings, $settings);
 	}
 
 	function startup(&$controller) {
-		if (!$this->Session->check('User.isMobile') || $this->force) {
-			$sClient = @new SoapClient('http://webservices.wapple.net/wapl.wsdl');
-			if($sClient) {
-				$headers = array();
-				foreach($_SERVER as $k => $v) {
-					$headers[] = array('name' => $k, 'value' => $v);
-				}
-				$params = array(
-					'devKey' => $this->devKey,
-					'deviceHeaders' => $headers
-				);
+		if (!$this->Session->check('User.isMobile') || $this->settings['force']) {
+			$headers = array();
+			foreach($_SERVER as $k => $v) {
+				$headers[] = array('name' => $k, 'value' => $v);
 			}
-			$isMobile = false;
-			if($sClient->isMobileDevice($params)) {
+			$this->params = array(
+				'devKey' => $this->devKey,
+				'deviceHeaders' => $headers
+			);
+		}
+		$isMobile = false;
+		if (extension_loaded('soap')) {
+			$this->Wapl = @new SoapClient($this->url.'wapl.wsdl');
+			if ($this->Wapl->isMobileDevice($params)) {
 				$isMobile = true;
 			}
-			if (!$this->force) {
-				$this->Session->write('User.isMobile', $isMobile);
+		} else {
+			App::import('Core', 'HttpSocket');
+			$this->Wapl = new HttpSocket();
+			$result = $this->Wapl->post($this->url.'isMobileDevice.php', $params);
+			if ($result) {
+				$isMobile = true;
 			}
 		}
-		if ($this->Session->read('User.isMobile') || $this->test) {
+		if (!$this->settings['force'] {
+			$this->Session->write('User.isMobile', $isMobile);
+		}
+		if ($this->Session->read('User.isMobile') || $this->settings['test']) {
 			$this->RequestHandler->respondAs('xml');
-			if ($this->path) {
-				$this->controller->layoutPath .= 'wapl';
-				$this->controller->viewPath .= DS.'wapl';
+			if ($this->settings['path']) {
+				$controller->layoutPath .= 'wapl';
+				$controller->viewPath .= DS.'wapl';
 			} else {
-				$this->controller->ext = '.wapl';
+				$controller->ext = '.wapl';
 			}
 		}
 	}
 
 	function beforeRender(&$controller) {
-		if ($this->Session->read('User.isMobile') || $this->test) {
+		if ($this->Session->read('User.isMobile') || $this->settings['test']) {
 			$helpers = array_diff($this->controller->helpers, array('Wapl.Wapl'));
-			$helpers['Wapl.Wapl'] = array('devKey' => $this->devKey, 'test' => $this->test);
 			$this->controller->helpers = $helpers;
+		}
+	}
+
+	function shutdown(&$controller) {
+		if ($this->settings['test'] != 'pre') {
+			$this->params['wapl'] = urlencode($controller->output);
+			if (extension_loaded('soap')) {
+				$xml = simplexml_load_string($this->Wapl->getMarkupFromWapl($this->params));
+			} else {
+				$xml = simplexml_load_string($this->Wapl->post($this->url.'getMarkupFromWapl.php', $params));
+			}
+			foreach ($xml->header->item as $v) {
+				header($v);
+			}
+			$controller->output = trim($xml->markup);
 		}
 	}
 
